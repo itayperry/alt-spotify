3045062c9815518aed4389e02a52b0aa6363ada3
Gil Tayar <gil@tayar.org>
2019-09-03 19:56:49 +0300

    PORT affects BASE_URL

--- scripts/run-alt-spotify.js
+++ scripts/run-alt-spotify.js
@@ -5,7 +5,9 @@ async function main() {
   if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET)
    throw new Error('you must specify CLIENT_ID and CLIENT_SECRET environment variables')
 
-  const externalBaseUrl = process.env.BASE_URL || `http://localhost:3000`
+  const port = process.env.PORT || 3000
+
+  const externalBaseUrl = process.env.BASE_URL || `http://localhost:${port}`
 
   const app = await makeApp({
     clientId: process.env.CLIENT_ID,
@@ -13,7 +15,7 @@ async function main() {
     externalBaseUrl
   })
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0')
+  await app.listen(port, '0.0.0.0')
   console.log(`Listening on port ${app.server.address().port} with external url ${externalBaseUrl}`)
 }
 
